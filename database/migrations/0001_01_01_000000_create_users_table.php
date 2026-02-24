1f47c3cb9e2483772a1fe95e6c36cba651a8b703
Khalid Elyounssi <elyounssiox2000@gmail.com>
2026-02-24 10:05:29 +0000

    feat: setup migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
+            $table->integer('reputation_score')->default(0); 
+            $table->boolean('is_admin')->default(false); 
+            
             $table->rememberToken();
             $table->timestamps();
         });
